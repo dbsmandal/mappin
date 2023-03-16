c8c65a4623ab48429fdf1f11b960ba5fe371687c
dbsmandal <dipakmandaljio420@gmail.com>
2023-03-17 04:58:29 +0530

    backend part done

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 const app=express();
+const userRoute=require('./routes/users');
+const pinRoute=require('./routes/pins');
+
 
 dotenv.config();
 
+app.use(express.json());
 mongoose
     .connect(process.env.MONGO_URL,{
         useNewUrlParser:true,
@@ -13,7 +17,10 @@ mongoose
     .then(()=>{
         console.log('MongoDB Connected!')
     })
-    .catch((error)=>console.log(error))
+    .catch((error)=>console.log(error));
+
+    app.use('/api/users',userRoute);
+    app.use('/api/pins',pinRoute);
 app.listen(8800,()=>{
     console.log('backend server is running!')
 })
