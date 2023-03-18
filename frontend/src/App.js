49bb006879e2fd7754623e1a08c90ad4ccb8f3db
dbsmandal <dipakmandaljio420@gmail.com>
2023-03-18 23:49:43 +0530

    configure the map in full screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map from 'react-map-gl';
 
 function App() {
   return (
-    <div className="App">
-     Hello world
-    </div>
+    <Map
+    
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{ width: '100vw', height: '100vh' }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
-
 export default App;
